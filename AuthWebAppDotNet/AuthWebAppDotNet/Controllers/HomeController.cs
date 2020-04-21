e12088fa3235c05e5ca6d2a2ef434891177309d9
Ajay Vyas <ajay.vyas@yash.com>
2020-04-21 16:50:59 +0530

    removed helper class

--- AuthWebAppDotNet/AuthWebAppDotNet/Controllers/HomeController.cs
+++ AuthWebAppDotNet/AuthWebAppDotNet/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace AuthWebAppDotNet.Controllers
     
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
